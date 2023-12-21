115cf79c99078be8e63b2d535cb5151a748756da
Harman Waheed <139279892+harmanpythanist@users.noreply.github.com>
2023-12-21 20:01:32 +0500

    model.py

--- model.py
+++ model.py
@@ -37,7 +37,7 @@ im = st.file_uploader('pick a file')
 
 def import_and_predict(image_data, model):
     size = (130,130)
-    image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
+    image = ImageOps.fit(image_data, size, Image.LANCZOS)
     img = np.asarray(image)
     img_reshape = img[np.newaxis, ..., cv2.IMREAD_GRAYSCALE]
     prediction = model.predict(img_reshape)
